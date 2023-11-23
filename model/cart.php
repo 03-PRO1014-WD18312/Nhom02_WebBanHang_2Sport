9353dfe18423c8feb19223302526404096b0109e
Lê Đình An <ledinhan@MacBook-Air-cua-Le-4.local>
2023-11-23 13:09:27 +0700

    a

--- model/cart.php
+++ model/cart.php
@@ -1,15 +1,36 @@
 <?php 
     //thêm giỏ hàng
-    function addcart($nameSp,$priceSp,$imgSp,$idkh,$id_variant,$idSize,$idColor){
-        $sql="INSERT INTO `cart`(`name`, `color`, `size`, `price`, `img`, `idAccount`, `idVariant`) VALUES ('$nameSp','$idColor','$idSize','$priceSp','$imgSp','$idkh','$id_variant')
+    function addcart($nameSp,$priceSp,$imgSp,$idkh,$id_variant){
+        $sql="INSERT INTO `cart`(`name`,`price`, `img`, `idAccount`, `idVariant`) VALUES ('$nameSp','$priceSp','$imgSp','$idkh','$id_variant')
         ";
         pdo_execute($sql);
     }
     //show giỏ hàng 
     function showcart($idkh){
-        $sql="SELECT * FROM cart WHERE idAccount = $idkh";
+        $sql="SELECT
+        cart.id AS cart_id,
+        product.id AS product_id,
+        product.name AS product_name,
+        product.img AS product_img,
+        variants.price AS variant_price,
+        variants.discount AS variant_discount,
+        product_size.size1,
+        product_color.color
+    FROM
+        cart
+    JOIN
+        variants ON cart.idVariant = variants.id
+    JOIN
+        product ON variants.idProduct = product.id
+    JOIN
+        product_size ON variants.idSize = product_size.id
+    JOIN
+        product_color ON variants.idColor = product_color.id
+    WHERE
+        cart.idAccount = $idkh;";
         $showcart=pdo_query($sql);
         return $showcart;
+
     }
     //xoá giỏ hàng
     function deletecart($id){
